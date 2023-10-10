38d35b5891ab6e8b0177e358f82e7e1ef5353255
Punyisa Arjhansiri <punyisaarjhansiri@gmail.com>
2023-10-10 11:41:26 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Components/Navbar.jsx'
+import Main from './Components/Main.jsx'
+
+
+function App() {
+  return (
+
+      <div>
+        <Navbar />
+        <Main />
+      </div>
+      
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png" />
+            <h3 className ="nav--logo_text">ReactFacts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
